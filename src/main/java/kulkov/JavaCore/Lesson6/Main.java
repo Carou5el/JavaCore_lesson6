a2a67ccdf559e68ff25254abb5142db5f6d9e3ee
Carou5el <garrydeepcool@outlook.com>
2021-02-08 14:32:26 +0500

    A 'full' parameter in 'Cat' class.

--- src/main/java/kulkov/JavaCore/Lesson6/Main.java
+++ src/main/java/kulkov/JavaCore/Lesson6/Main.java
@@ -3,5 +3,17 @@ package kulkov.JavaCore.Lesson6;
 public class Main {
     public static void main(String[] args) {
 
+        Cat cat = new Cat("Barsik", 5);
+        Plate plate = new Plate(100);
+
+        // Вывод инфы об объекте "Тарелка".
+        plate.info();
+        //
+        cat.eat(plate);
+        plate.info();
+
+
+
+
     }
 }
--- src/main/java/kulkov/JavaCore/Lesson6/Plate.java
+++ src/main/java/kulkov/JavaCore/Lesson6/Plate.java
@@ -1,4 +1,29 @@
 package kulkov.JavaCore.Lesson6;
 
 public class Plate {
+
+    private int food;
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    // Constructors.
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    // Methods.
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+
+    public void decreaseFood(int n) {
+
+        if(n > food)    {
+            System.out.printf("Insufficient balance!\n");
+        } else {
+            food -= n;
+        }
+    }
 }
